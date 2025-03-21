6ef0e3e89269ce3f64881e81241e1e3d7b2ffb33
Tuan Pham <33243797+JackoPham@users.noreply.github.com>
2025-03-21 21:15:10 +0700

    add homepage

---
+++ lib/safe_redirect_rails/configuration.rb
@@ -0,0 +1,33 @@
+module SafeRedirectRails
+  class << self
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+  end
+
+  class Configuration
+    attr_accessor :default_path, :whitelist_local, :log
+    attr_reader :domain_whitelists
+
+    def initialize
+      self.default_path = '/'
+      self.whitelist_local = false
+      self.domain_whitelists = []
+      self.log = false
+    end
+
+    def domain_whitelists=(whitelists)
+      if whitelists.any?{ |w| w =~ /\*\z/ }
+        raise ArgumentError, "whitelisted domain cannot end with a glob (*)"
+      end
+
+      @domain_whitelists = whitelists
+    end
+  end
+end
---
+++ lib/safe_redirect_rails/safe_redirect_rails.rb
@@ -0,0 +1,102 @@
+require 'uri'
+
+module SafeRedirectRails
+  extend self
+
+  def safe_domain?(uri)
+    return true if valid_uri?(uri)
+    return false if uri.host.nil?
+
+    SafeRedirect.configuration.domain_whitelists.any? do |domain|
+      if domain.include?("*")
+        rf = domain.split(/(\*)/).map{ |f| f == "*" ? "[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9]?" : Regexp.escape(f) }
+        regexp = Regexp.new("\\A#{rf.join}\\z")
+
+        safe = uri.host.match(regexp)
+
+        # if domain starts with *. and contains no other wildcards, include the
+        # naked domain too (e.g. foo.org when *.foo.org is the whitelist)
+        if domain =~ /\A\*\.[^\*]+\z/
+          naked_domain = domain.gsub("*.", "")
+          safe || uri.host == naked_domain
+        else
+          safe
+        end
+      else
+        uri.host == domain
+      end
+    end
+  end
+
+  def safe_path(path)
+    case path
+    when String
+      clean_path(path)
+    when Hash
+      sanitize_hash(path)
+    else
+      path
+    end
+  end
+
+  def redirect_to(path, options={})
+    target = options[:safe] ? path : safe_path(path)
+
+    log("Unsafe redirect path modified to #{target} from #{path}", :warn) if target != path
+    
+    default_options = { allow_other_host: true }
+    merged_options = default_options.merge(options)
+    super target, default_options
+  rescue NoMethodError
+  end
+
+  private
+
+  def clean_path(path)
+    uri = URI.parse(path)
+    valid_path?(path) && safe_domain?(uri) ? path : SafeRedirect.configuration.default_path
+  rescue URI::InvalidURIError
+    SafeRedirect.configuration.default_path
+  end
+
+  def sanitize_hash(hash)
+    protocol = hash[:protocol] || 'http'
+    host = hash[:host]
+    uri = URI.parse("#{protocol}://#{host}")
+    hash.delete(:host) unless safe_domain?(uri)
+    hash
+  end
+
+  def valid_uri?(uri)
+    return true if uri.host && whitelist_local? && local_address?(uri.host)
+    return false unless uri.host.nil? && uri.scheme.nil?
+    return true if uri.path.nil? || uri.path =~ /^\//
+    false
+  end
+
+  def valid_path?(path)
+    path !~ /\/\/\//
+  end
+
+  def whitelist_local?
+    SafeRedirect.configuration.whitelist_local
+  end
+
+  # borrowed the regex from https://github.com/rack/rack/blob/ea9e7a570b7ffd8ac6845a9ebecdd7de0af6b0ca/lib/rack/request.rb#L420
+  def local_address?(host)
+    host =~ /\A127\.0\.0\.1\Z|\A(10|172\.(1[6-9]|2[0-9]|30|31)|192\.168)\.|\A::1\Z|\Afd[0-9a-f]{2}:.+|\Alocalhost\Z|\Aunix\Z|\Aunix:/i
+  end
+
+  def log(msg, level = :warn)
+    return unless (logger = SafeRedirect.configuration.log)
+
+    msg = "[#{Time.now}] SafeRedirect: #{msg}"
+
+    if logger.respond_to?(level)
+      logger.send(level, msg)
+    elsif defined?(Rails)
+      Rails.logger.send(level, msg)
+    end
+  end
+
+end
