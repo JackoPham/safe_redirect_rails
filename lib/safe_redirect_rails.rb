b2620d62c5be47b84bb51bbfd87b20d1d1545219
Tuan Pham <33243797+JackoPham@users.noreply.github.com>
2025-03-21 21:35:09 +0700

    export class

--- lib/safe_redirect_rails.rb
+++ lib/safe_redirect_rails.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 require 'safe_redirect_rails/version'
-require 'safe_redirect_rails/safe_redirect'
+require 'safe_redirect_rails/safe_redirect_rails'
 require 'safe_redirect_rails/configuration'
